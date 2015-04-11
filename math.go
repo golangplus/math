2e2a06089a3f431c1afb974efa610a84214b22b7
David Deng <daviddengcn@gmail.com>
2015-04-10 21:57:21 -0700

    Comments

--- math.go
+++ math.go
@@ -1,3 +1,10 @@
+// Copyright 2015 The Golang Plus Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+Package mathp is a plus to standard "math" package.
+*/
 package mathp
 
 // MaxI returns the larger of x or y.
