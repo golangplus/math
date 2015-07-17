7a6d3a3b9ee231a8dc65cab4372b514d26230bca
David Deng <daviddengcn@gmail.com>
2015-07-17 00:29:20 -0700

    ADD examples

--- math_test.go
+++ math_test.go
@@ -18,11 +18,40 @@ func TestMin(t *testing.T) {
 	}), 2)
 }
 
+func ExampleMinStrings() {
+	names := []string{
+		"David",
+		"Richard",
+		"Sophie",
+		"Mengxi",
+	}
+	minIndex := Min(len(names), func(i, j int) bool {
+		return names[i] < names[j]
+	})
+	fmt.Println(minIndex, names[minIndex])
+
+	// Output:
+	// 0 David
+}
+
 func TestMax(t *testing.T) {
 	data := []int{5, 7, 1, 9, 3}
 	assert.Equal(t, "max", Max(len(data), func(i, j int) bool {
 		return data[i] < data[j]
 	}), 3)
+}
+
+func ExampleMaxFloat() {
+	floats := []float64{
+		2.5, 3.1, 1.7, 0.5,
+	}
+	maxIndex := Max(len(floats), func(i, j int) bool {
+		return floats[i] < floats[j]
+	})
+	fmt.Println(maxIndex)
+
+	// Output:
+	// 1
 }
 
 func TestMaxI(t *testing.T) {
